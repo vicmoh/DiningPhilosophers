70d5330f6b21b8d1c8b4c0c8c146889dbfe86c07
vicmoh <vimoh1213@gmail.com>
2018-03-23 03:56:35 -0400

    part 1 complete

--- src/dine.c
+++ src/dine.c
@@ -2,6 +2,7 @@
  * Vicky Mohammad
  * 0895381
  * March 22, 2018
+ * reference: https://www.geeksforgeeks.org/dining-philosophers-solution-using-monitors/
  ********************************************/
 
 #include <stdio.h>
@@ -10,35 +11,38 @@
 #include <unistd.h>
 #include <pthread.h>
 #define TRUE 1
-#define FALSE 2
+#define FALSE 0
 #define INVERT 1
-#define NORMAL 2
+#define NORMAL 0
 #define NUMBER_RANGE( var ) (var >= 48 && var <= 57)
-#define debug if(TRUE)printf
+#define debug if(FALSE)printf
 
 /********************************************
  * header
  ********************************************/
 
+//dec forks on the table
+pthread_mutex_t* forks;
+//dec object philosopher
 typedef struct{
     int philNumber;
-    int lowFork;
+    int downFork;
     int upFork;
     int numOfEats;
-    int forkType;
+    int forkSelection;
 }Philosopher;
-
+//dec dine that will have phil eating
 typedef struct{
     Philosopher** phil;
-    pthread_mutex_t* forks;
     pthread_t* tempThread;
-}Instance;
-
+}Dine;
+//functions/methods
 Philosopher* newPhilosopher();
-Instance* newInstance(int numOfPhils);
-void initPThreadMutex(Instance* vars, int numOfPhils);
+Dine* newDine(int numOfPhils);
 int checkIfNumbers(char* string);
-void runSimulation(Instance* vars, int numOfPhils, int numOfEats);
+void runSimulation(Dine* vars, int numOfPhils, int numOfEats);
+void* philosopherDiner(void* arg);
+int waiting();
 
 /********************************************
  * main
@@ -52,9 +56,9 @@ int main(int argc, char **argv){
     int numOfEats = 0;
     debug("argc = %d\n", argc);
 
-    //error check
-    if(argc == 3){
-        printf("There must be 3 arguments...\n");
+    //error checking if num of arg is not 3 and not number
+    if(argc != 3){
+        printf("There must be 2 arguments...\n");
         printf("Please re-run the program..\n");
         printf("Exiting program...\n");
         return 0;
@@ -69,11 +73,13 @@ int main(int argc, char **argv){
     //convert the arg to int
     numOfPhils = atoi(numOfPhilsInString);
     numOfEats = atoi(numOfEatsInString);
-    Instance* vars = newInstance(numOfPhils);
-    initPThreadMutex(vars, numOfPhils);
+    Dine* vars = newDine(numOfPhils);
     
     //run the program
-    
+    runSimulation(vars, numOfPhils, numOfEats);
+    printf("Simulation complete.\n");
+    printf("All %d philosophers are done eating!\n", numOfPhils);
+    return 0;
 }//end main
 
 /********************************************
@@ -81,22 +87,23 @@ int main(int argc, char **argv){
  ********************************************/
 
 Philosopher* newPhilosopher(){
+    //create new philosopher
     Philosopher* new = malloc(sizeof(Philosopher));
     new->philNumber = 0;
-    new->lowFork = 0;
+    new->downFork = 0;
     new->upFork = 0;
     new->numOfEats = 0;
-    new->forkType = 0;
+    new->forkSelection = 0;
     return new;
 }//end constructor
 
-Instance* newInstance(int numOfPhils){
-    //init instance to act as global
-    Instance* new = malloc(sizeof(Instance));
-    // init phil
+Dine* newDine(int numOfPhils){
+    //init Dine to act as global
+    Dine* new = malloc(sizeof(Dine));
+
+    // initilizie new philosophers
     // For each philosopher, 
-    //assign it two forks such that phil i get forks i-1 
-    //and i for all i>0. If i=0, take fork0 and forkMax
+    //assign it two forks for picking the nearest
     new->phil = calloc(numOfPhils, sizeof(Philosopher*));
     for(int x=0; x<numOfPhils; x++){
         new->phil[x] = newPhilosopher();
@@ -104,27 +111,24 @@ Instance* newInstance(int numOfPhils){
         new->phil[x]->upFork = x;
         new->phil[x]->numOfEats = 0;
         if(x == 0){
-            new->phil[x]->lowFork = numOfPhils-1;
+            new->phil[x]->downFork = numOfPhils-1;
         }else if(x != 0){
-            new->phil[x]->lowFork = x-1;
+            new->phil[x]->downFork = x-1;
         }//end if
     }//end if
-    //init forks
-    new->forks = calloc(numOfPhils, sizeof(pthread_mutex_t));
+
+    //initilizie the number of forks availble based on the number of philosophers
+    forks = calloc(numOfPhils, sizeof(pthread_mutex_t));
+    for(int x=0; x<numOfPhils; x++){
+        pthread_mutex_init(&forks[x], NULL);
+    }//end if
     new->tempThread = calloc(numOfPhils, sizeof(pthread_t));
     return new;
 }//end constructor
 
 /********************************************
- * functions
+ * functions/methods
  ********************************************/
-
-void initPThreadMutex(Instance* vars, int numOfPhils){
-    //init the pthread mutex
-    for(int x=0; x<numOfPhils; x++){
-        pthread_mutex_init(&vars->forks[x], NULL);
-    }//end for
-}//end func
 
 int checkIfNumbers(char* string){
     //make sure it doesnt start at 0
@@ -141,22 +145,60 @@ int checkIfNumbers(char* string){
     return TRUE;
 }//end func
 
-void runSimulation(Instance* vars, int numOfPhils, int numOfEats){
+void runSimulation(Dine* vars, int numOfPhils, int numOfEats){
+    //run the simlation
+    debug("running sim()\n");
     //assign the relevent vars to the object being passed into the thread
     for(int x=0; x<numOfPhils; x++){
         vars->phil[x]->numOfEats = numOfEats;
         if(numOfPhils-1 == x){
-            vars->phil[x]->forkType = INVERT;
+            vars->phil[x]->forkSelection = INVERT;
         }//end if
-        //pthread_create(&vars->tempThread[x], NULL, &);
+        pthread_create(&vars->tempThread[x], NULL, &philosopherDiner, &vars->phil[x]);
     }//end for
 
-    for(int x =0; x<numOfPhils; x++){
-        pthread_join(vars->tempThread[x], NULL);
+    //thread cache
+    for(int x=0; x<numOfPhils; x++){
+        pthread_join(vars->tempThread[x],NULL);
     }//end for
     free(vars->tempThread);
 }//end func
 
-void* philosopherEating(void* arg){
-    return NULL;
+void* philosopherDiner(void* arg){
+    debug("calling philosopherDiner()\n");
+    //set the current phil that is waiting to eat
+    Philosopher* phil = *((Philosopher**)arg);
+
+    //For loop as many times as they have to eat
+    for(int x=0; x<phil->numOfEats; x++){
+        //let the phil grab the forks
+        if(phil->forkSelection == INVERT){
+            waiting();
+            printf("Philosopher %d is thinking...\n", phil->philNumber);
+            pthread_mutex_lock(&forks[phil->upFork]);
+            pthread_mutex_lock(&forks[phil->downFork]);
+        }else if(phil->forkSelection == NORMAL){
+            waiting();
+            printf("Philosopher %d is thinking...\n", phil->philNumber);
+            pthread_mutex_lock(&forks[phil->downFork]);
+            pthread_mutex_lock(&forks[phil->upFork]);
+        }//end if
+        waiting();
+
+        //time for the person to eat
+        printf("Philosopher %d is eating...\n", phil->philNumber);
+        pthread_mutex_unlock(&forks[phil->downFork]);
+        pthread_mutex_unlock(&forks[phil->upFork]);
+    }//end for
+    pthread_exit(NULL);
 }//end func
+
+int waiting(){
+    //create a waiting time by using sleep
+    //the waiting time will be random from 1 to 2 sleep
+    debug("calling waiting()\n");
+    int randomNum = rand() % 2;
+    int waitTime = sleep(randomNum + 1);
+    debug("wait time = %d\n", waitTime);
+    return waitTime;
+}//end func 
